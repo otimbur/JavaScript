ead3c5ebd70247b4fc441cb6e1d575798b5a0023
otimbur <otimbur@sea.yearup.org>
2022-06-07 13:53:12 -0700

    finish

--- javaScript/conditionals.js
+++ javaScript/conditionals.js
@@ -18,15 +18,26 @@
 // create an if/else statement
 const dayOfWeek = 'Monday';
 //If the first condition is false
-if (dayOfWeek === 'Sunday') {
-    console.log('Today is Sunday')
-    // it will check the else if condition 
-} else if (dayOfWeek === 'Tuesday') {
-    console.log('Today is not Tusday')
+// if (dayOfWeek === 'Sunday') {
+//     console.log('Today is Sunday')
+//     // it will check the else if condition 
+// } else if (dayOfWeek === 'Tuesday') {
+//     console.log('Today is not Tusday')
 
-} else if (dayOfWeek === 'Friday') {
-    console.log('Today is Friday')
-    // rhe condition will match so will run the code.
-} else if (dayOfWeek === 'Monday') {
-    console.log('Today is Monday')
-}
+// } else if (dayOfWeek === 'Friday') {
+//     console.log('Today is Friday')
+//     // rhe condition will match so will run the code.
+// } else if (dayOfWeek === 'Monday') {
+//     console.log('Today is Monday')
+// }
+
+// using promt to imput the value of the variable
+// const age = prompt('Enter your age')
+
+// if (age < 5) {
+//     console.log('Your are less that 5')
+// } else if (age < 10) {
+//     console.log('Still to young to drive')
+// } else if (age >= 16) {
+//     console.log('you are able to drive')
+// }
